ea96e232c3a66602f8057a4cf8e4ca01c7159640
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2021-03-10 13:35:14 +0300

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -85,10 +85,60 @@ window.addEventListener("DOMContentLoaded", () => {
             seconds.innerHTML = getZero(t.seconds);
 
             if (t.total <= 0) {
+                days.innerHTML = getZero(0);
+                hours.innerHTML = getZero(0);
+                minutes.innerHTML = getZero(0);
+                seconds.innerHTML = getZero(0);
                 clearInterval(timeInterval);
             }
         }
     }
 
     setClock('.timer', deadline);
+
+    /* Modal */
+
+    const   modalOpenBtn = document.querySelectorAll('[data-modal]'),
+            modal = document.querySelector('.modal'),
+            modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    function closeModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalOpenBtn.forEach(item => {
+        item.addEventListener('click', openModal)
+    })
+
+    modalCloseBtn.addEventListener('click', closeModal)
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal()
+        }
+    })
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 })
